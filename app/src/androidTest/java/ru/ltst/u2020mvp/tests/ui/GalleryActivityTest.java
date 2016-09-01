9e94be44d644e4f261a4d80a6cc8dbe2e6dd84e5
Gleb Eremeev <g.eremeev@ltst.ru>
2016-09-01 20:11:31 +0600

    #Test revrite gallery activity test

--- app/src/androidTest/java/ru/ltst/u2020mvp/tests/ui/GalleryActivityTest.java
+++ app/src/androidTest/java/ru/ltst/u2020mvp/tests/ui/GalleryActivityTest.java
@@ -1,6 +1,7 @@
 package ru.ltst.u2020mvp.tests.ui;
 
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
@@ -12,6 +13,7 @@ import org.junit.runner.RunWith;
 import ru.ltst.u2020mvp.R;
 import ru.ltst.u2020mvp.tests.util.ActivityRule;
 import ru.ltst.u2020mvp.tests.util.Constants;
+import ru.ltst.u2020mvp.tests.util.TimerTestRule;
 import ru.ltst.u2020mvp.tests.util.ViewActions;
 import ru.ltst.u2020mvp.ui.gallery.GalleryActivity;
 
@@ -26,40 +28,24 @@ import static junit.framework.Assert.assertNotNull;
 public class GalleryActivityTest {
 
     @Rule
-    public final ActivityRule<GalleryActivity> main = new ActivityRule<>(GalleryActivity.class);
-
-    private GalleryActivity activity;
-
-    @Before
-    public void setUp() {
-        activity = main.get();
-    }
-
-    @After
-    public void tearDown() {
-        activity = null;
-    }
-
-    @Test
-    public void checkPreconditions() {
-        assertNotNull(activity);
-    }
+    public final ActivityTestRule<GalleryActivity> main = new ActivityTestRule<>(GalleryActivity.class);
+    @Rule
+    public final TimerTestRule timerTestRule = new TimerTestRule();
 
     @Test
     public void loadData() {
         String tag = "1. Root view visible, progress bar showing";
         onView(isRoot()).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
         tag = "2. Grid shown with images loaded";
-        // TODO: remove wait delay, use RxJava's TestScheduler or Espresso.registerIdlingResource
-        onView(isRoot()).perform(ViewActions.waitAtLeast(Constants.WAIT_DELAY));
-        onView(withId(R.id.gallery_grid)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        timerTestRule.scheduleTimeout(Constants.WAIT_DELAY);
+        onView(withId(R.id.gallery_grid))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 
     @Test
     public void listScrolling() {
         String tag = "1. Grid shown with images loaded";
-        // TODO: remove wait delay, use RxJava's TestScheduler or Espresso.registerIdlingResource
-        onView(isRoot()).perform(ViewActions.waitAtLeast(Constants.WAIT_DELAY));
+        timerTestRule.scheduleTimeout(Constants.WAIT_DELAY);
         onView(withId(R.id.gallery_grid)).perform(ViewActions.swipeTop());
         tag = "2. Scrolled to bottom";
         tag = "3. Scrolled to top";
