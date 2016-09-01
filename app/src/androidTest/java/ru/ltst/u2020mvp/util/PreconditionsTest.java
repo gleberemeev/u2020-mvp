0dd076f118c938fa31aa8c9578d0efc80d5b69a2
Gleb Eremeev <g.eremeev@ltst.ru>
2016-09-01 21:21:06 +0600

    #Test added preconditions test

---
+++ app/src/androidTest/java/ru/ltst/u2020mvp/util/PreconditionsTest.java
@@ -0,0 +1,38 @@
+package ru.ltst.u2020mvp.util;
+
+import org.hamcrest.CustomTypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.internal.matchers.ThrowableMessageMatcher;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
+public class PreconditionsTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void checkNotNull() throws Exception {
+        Integer ref = 1;
+        Preconditions.checkNotNull(ref);
+        expectedException.expect(NullPointerException.class);
+        Preconditions.checkNotNull(null);
+    }
+
+    @Test
+    public void checkNotNull1() throws Exception {
+        Integer ref = 1;
+        Preconditions.checkNotNull(ref);
+        expectedException.expect(new ThrowableMessageMatcher<NullPointerException>(
+                new CustomTypeSafeMatcher<String>("exception message matcher") {
+            @Override
+            protected boolean matchesSafely(String item) {
+                return item.equals("message");
+            }
+        }));
+        Preconditions.checkNotNull(null, "message");
+    }
+
+}
