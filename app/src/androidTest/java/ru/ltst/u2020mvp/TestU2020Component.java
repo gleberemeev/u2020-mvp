f8d4404276b0571be5da7bf3085bc4e7894717fb
Gleb Eremeev <g.eremeev@ltst.ru>
2016-09-13 21:18:41 +0600

    #Test Added external activity test

--- app/src/androidTest/java/ru/ltst/u2020mvp/TestU2020Component.java
+++ app/src/androidTest/java/ru/ltst/u2020mvp/TestU2020Component.java
@@ -3,10 +3,13 @@ package ru.ltst.u2020mvp;
 import dagger.Component;
 import ru.ltst.u2020mvp.data.DebugDataModule;
 import ru.ltst.u2020mvp.ui.DebugUiModule;
+import ru.ltst.u2020mvp.ui.ExternalIntentActivityTest;
 import ru.ltst.u2020mvp.ui.screen.main.MainActivityTest;
 
 @ApplicationScope
 @Component(modules = {U2020AppModule.class, DebugUiModule.class, DebugDataModule.class, TestU2020Module.class})
 public interface TestU2020Component extends U2020Component {
     void inject(MainActivityTest mainActivityTest);
+
+    void inject(ExternalIntentActivityTest externalIntentActivityTest);
 }
